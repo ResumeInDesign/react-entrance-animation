cad574e7ef4c3722d39250ae164c8e91647c9a50
Yasser Hennawi <yahennawi@gmail.com>
2018-12-09 12:07:22 +0200

    init create-react-library@2.6.7

--- src/EntranceAnimation/test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
-import EntranceAnimation from './index';
-
-const TestEntranceAnimation = () => <EntranceAnimation />;
-
-export default hot(module)(TestEntranceAnimation);
